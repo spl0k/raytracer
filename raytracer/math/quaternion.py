bf02a51125a783c213348bae3d38dd54d77352d9
Alban Féron <alban.feron@gmail.com>
2020-05-10 15:08:46 +0200

    Some quaternion math
    With a fix of Euler conversions

--- raytracer/math/quaternion.py
+++ raytracer/math/quaternion.py
@@ -1,6 +1,7 @@
 import math
 
 from dataclasses import dataclass
+from typing import Union, overload
 
 from .vector import Vector3
 
@@ -16,19 +17,19 @@ class Quaternion:
 
     @property
     def euler(self) -> Vector3:
-        sinx = 2 * (self.w * self.y - self.z * self.x)
-        if abs(sinx) >= 1:
-            x = math.copysign(math.pi * 0.5, sinx)
+        sinx_cosy = 2 * (self.w * self.x + self.y * self.z)
+        cosx_cosy = 1 - 2 * (self.x * self.x + self.y * self.y)
+        x = math.atan2(sinx_cosy, cosx_cosy)
+
+        siny = 2 * (self.w * self.y - self.z * self.x)
+        if abs(siny) >= 1:
+            y = math.copysign(math.pi * 0.5, siny)
         else:
-            x = math.asin(sinx)
+            y = math.asin(siny)
 
-        siny_cosx = 2 * (self.w * self.z + self.x * self.y)
-        cosy_cosx = 1 - 2 * (self.y * self.y + self.z * self.z)
-        y = math.atan2(siny_cosx, cosy_cosx)
-
-        sinz_cosx = 2 * (self.w * self.x + self.y * self.z)
-        cosz_cosx = 1 - 2 * (self.x * self.x + self.y * self.y)
-        z = math.atan2(sinz_cosx, cosz_cosx)
+        sinz_cosy = 2 * (self.w * self.z + self.x * self.y)
+        cosz_cosy = 1 - 2 * (self.y * self.y + self.z * self.z)
+        z = math.atan2(sinz_cosy, cosz_cosy)
 
         return Vector3(math.degrees(x), math.degrees(y), math.degrees(z))
 
@@ -45,12 +46,74 @@ class Quaternion:
         cz = math.cos(euler.z * 0.5)
         sz = math.sin(euler.z * 0.5)
 
-        w = cz * cx * cy + sz * sx * sy
-        x = sz * cx * cy - cz * sx * sy
-        y = cz * sx * cy + sz * cx * sy
-        z = cz * cx * sy - sz * sx * cy
+        w = cx * cy * cz + sx * sy * sz
+        x = sx * cy * cz - cx * sy * sz
+        y = cx * sy * cz + sx * cy * sz
+        z = cx * cy * sz - sx * sy * cz
 
         return Quaternion(x, y, z, w)
+
+    @property
+    def sqrlength(self) -> float:
+        return self.x ** 2 + self.y ** 2 + self.z ** 2 + self.w ** 2
+
+    @property
+    def length(self) -> float:
+        return math.sqrt(self.sqrlength)
+
+    @property
+    def normalized(self) -> "Quaternion":
+        sqrlen = self.sqrlength
+        if math.isclose(sqrlen, 1):
+            return self
+
+        len = math.sqrt(sqrlen)
+        return Quaternion(self.x / len, self.y / len, self.z / len, self.w / len)
+
+    @overload
+    def __mul__(self, other: float) -> "Quaternion":
+        ...
+
+    @overload
+    def __mul__(self, other: "Quaternion") -> "Quaternion":
+        ...
+
+    @overload
+    def __mul__(self, other: Vector3) -> Vector3:
+        ...
+
+    def __mul__(self, other):
+        if isinstance(other, float):
+            return self.__float_mul(other)
+        if isinstance(other, Quaternion):
+            return self.__quat_mul(other)
+        if isinstance(other, Vector3):
+            return self.__vec_mul(other)
+        return NotImplemented
+
+    def __float_mul(self, other: float) -> "Quaternion":
+        return Quaternion(
+            self.x * other, self.y * other, self.z * other, self.w * other
+        )
+
+    def __quat_mul(self, other: "Quaternion") -> "Quaternion":
+        x = self.w * other.x + other.w * self.x + self.y * other.z - other.y * self.z
+        y = self.w * other.y + other.w * self.y + self.z * other.x - other.z * self.x
+        z = self.w * other.z + other.w * self.z + self.x * other.y - other.x * self.y
+        w = self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z
+        return Quaternion(x, y, z, w)
+
+    def __vec_mul(self, other: Vector3) -> Vector3:
+        p = self.normalized
+        pc = Quaternion(-p.x, -p.y, -p.z, p.w)
+        q = Quaternion(other.x, other.y, other.z, 0)
+        rv = p * q * pc
+        return Vector3(rv.x, rv.y, rv.z)
+
+    def __rmul__(self, other: float) -> "Quaternion":
+        return Quaternion(
+            self.x * other, self.y * other, self.z * other, self.w * other
+        )
 
 
 identity = Quaternion(0, 0, 0, 1)
