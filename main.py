527c06ce390daffafe9c5c4bb5844075b8ac83ad
Alban Féron <alban.feron@gmail.com>
2020-05-14 22:13:11 +0200

    Saving resulting images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+import os.path
 
 from raytracer.raycaster import Raycaster
 from raytracer.sceneloader import load_scene
@@ -8,7 +9,7 @@ from raytracer.sceneloader import load_scene
 @click.argument("infile", type=click.Path(dir_okay=False))
 @click.argument("width", type=int)
 @click.argument("height", type=int)
-@click.argument("outfile")
+@click.argument("outfile", type=click.Path(dir_okay=False))
 def main(infile: str, outfile: str, width: int, height: int):
     scene = load_scene(infile)
     caster = Raycaster(scene)
@@ -16,6 +17,17 @@ def main(infile: str, outfile: str, width: int, height: int):
         cam.generate_initial_rays(width, height, caster, scene.background)
 
     caster.process()
+
+    name, suffix = os.path.splitext(outfile)
+    if not suffix:
+        suffix = ".png"
+    if len(scene.cameras) > 1:
+        name_format = f"{name}_{{}}{suffix}"
+    else:
+        name_format = f"{name}{suffix}"
+
+    for i, cam in enumerate(scene.cameras):
+        cam.image.save(name_format.format(i))
 
 
 if __name__ == "__main__":
